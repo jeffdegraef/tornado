ffbf29c7c72cead22c99b5357f8815380a40bc18
jeffdegraef <jeffdegraef@gmail.com>
2016-07-14 22:19:19 +0200

    # Added Full Norsonic site

--- build/HelloWorld.py
+++ build/HelloWorld.py
@@ -6,11 +6,14 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
+class NorsonicHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("indexNorsonic.html")
 
 def make_app():
     settings = {"static_path": os.path.join(os.path.dirname(__file__), "")}
     return tornado.web.Application([
-        (r"/", MainHandler)
+        (r"/", MainHandler),(r"/Norsonic", NorsonicHandler)
     ], **settings)
 
 if __name__ == "__main__":
