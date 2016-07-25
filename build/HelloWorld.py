93a633723ae570e7cb591a9210951703ed0584de
jeffdegraef <jeffdegraef@gmail.com>
2016-07-25 18:56:21 +0200

    # Full rebuild to REACT framework for Norsonic site # Updated start Norsonic site, now all working # todo, separate inline js code

--- build/HelloWorld.py
+++ build/HelloWorld.py
@@ -17,7 +17,10 @@ class TestHandler(tornado.web.RequestHandler):
 def make_app():
     settings = {"static_path": os.path.join(os.path.dirname(__file__), "")}
     return tornado.web.Application([
-        (r"/", MainHandler), (r"/Norsonic", NorsonicHandler), (r"/test", TestHandler), (r"/js/(.*)", tornado.web.StaticFileHandler, {"path": "./js"}), (r"/images/(.*)", tornado.web.StaticFileHandler, {"path": "./images"}), (r"/(.*)", tornado.web.StaticFileHandler, {"path": "./"}),
+        (r"/", MainHandler), (r"/Norsonic", NorsonicHandler),
+        (r"/test", TestHandler), (r"/js/(.*)", tornado.web.StaticFileHandler, {"path": os.path.join(os.path.dirname(__file__), "./js")}),
+        (r"/images/(.*)", tornado.web.StaticFileHandler, {"path": os.path.join(os.path.dirname(__file__), "./images")}),
+        (r"/(.*)", tornado.web.StaticFileHandler, {"path": os.path.join(os.path.dirname(__file__), "./")}),
     ], **settings)
 
 if __name__ == "__main__":
